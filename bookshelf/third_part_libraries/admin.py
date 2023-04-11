cc993747f59482896a8b390bb1153a964bbab013
Olexandr Brailovskyi <alexander.braylovskiy@gmail.com>
2023-04-11 17:37:35 +0300

    Added link to author to flibusta author admin model

--- bookshelf/third_part_libraries/admin.py
+++ bookshelf/third_part_libraries/admin.py
@@ -3,17 +3,35 @@ from django.contrib import admin
 from third_part_libraries.models import FlibustaAuthor
 
 
+class FlibustaAuthorInline(admin.StackedInline):
+    model = FlibustaAuthor
+    fk_name = 'main_author'
+    # readonly_fields = ('FlibustaAuthor',)
+    can_delete = False
+    extra = 0
+
+    # def has_add_permission(self, request, obj=None):
+    #     return False
+    #
+    # def has_delete_permission(self, request, obj=None):
+    #     return False
+
+
 class FlibustaAuthorAdmin(admin.ModelAdmin):
-    # fields = ("title", "text", "tags")
+    fields = ('last_name', 'first_name', 'middle_name', 'main_author', 'library_author')
     ordering = ('id',)
 
     list_display = ('id', 'last_name', 'first_name', 'middle_name', 'main_author')
-    list_display_links = list_display
+    list_display_links = ('id', 'last_name', 'first_name', 'middle_name', 'main_author')
 
     search_fields = ('id', 'last_name', 'middle_name', 'first_name',)
 
     autocomplete_fields = ('main_author',)
     list_select_related = ('main_author',)
+
+    # inlines = [FlibustaAuthorInline, ]
+
+    readonly_fields = ('library_author', )
 
     def get_queryset(self, request):
         # add filter for autocomplete field 'main_author'
@@ -22,6 +40,12 @@ class FlibustaAuthorAdmin(admin.ModelAdmin):
             return super().get_queryset(request).filter(main_author=None).select_related('main_author')
 
         return super().get_queryset(request).select_related('main_author')
+
+    # def get_readonly_fields(self, request, obj=None):
+    #     if request.user.is_superuser:
+    #         return self.readonly_fields
+    #
+    #     return self.fields
 
 
 admin.site.register(FlibustaAuthor, FlibustaAuthorAdmin)
