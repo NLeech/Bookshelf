8f588bf2ad41a79b684009e66333684c852a1d34
Olexandr Brailovskyi <alexander.braylovskiy@gmail.com>
2025-08-27 23:50:44 +0300

    feat: Add index page
    BOOK-6. Set up the initial Django project structure.

--- bookshelf/bookshelf/settings.py
+++ bookshelf/bookshelf/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'authentication.apps.AuthenticationConfig',
+    'library.apps.LibraryConfig',
 
     'debug_toolbar',
     'crispy_forms',
--- bookshelf/bookshelf/urls.py
+++ bookshelf/bookshelf/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
+    path('', include('library.urls')),
     path('accounts/', include('allauth.urls')),
     path('admin/', admin.site.urls),
 ]  + debug_toolbar_urls()
