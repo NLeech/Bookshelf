a8bf65d82ce5874719175f3e6ba03d4c289946fc
Olexandr Brailovskyi <alexander.braylovskiy@gmail.com>
2026-03-04 00:46:52 +0200

    feat: implement alphabet tree structure for authors grouped by last name

--- bookshelf/library/sevices.py
+++ bookshelf/library/sevices.py
@@ -1,8 +1,133 @@
 import logging
+from dataclasses import dataclass, field
+from sys import prefix
+
 from django.db import transaction
+from django.db.models import Count
+from django.db.models.functions import Left, Lower
 
 from third_party_libraries.models import FlibustaAuthor, FlibustaGenre
 from library.models import Author, Genre
+
+
+MAX_TREE_DEPTH = 3
+
+
+@dataclass(order=True)
+class AlphabetTree:
+    """
+    A tree structure for storing authors grouped by the first letters of their last names.
+    """
+    name: str = field(compare=True)
+    regex: str = field(default='', compare=False, repr=False)
+    authors_quantity: int = field(default=0, compare=False, repr=True)
+    entries: list['AlphabetTree'] = field(default_factory=list, compare=False, repr=False)
+
+    def __str__(self):
+        return f'{self.name.capitalize()}'
+
+
+def get_alphabet_tree() -> AlphabetTree:
+    """
+    Get a tree structure for storing authors grouped by the first letters of their last names.
+    :return: the root of the tree
+    """
+
+    root = AlphabetTree(name='')
+
+    # first level -
+    # by the first letter of the last name
+    results = (
+        Author.objects
+        .filter(last_name__regex=r'^[[:alpha:]]')
+        .annotate(name = Left(Lower('last_name'), 1))
+        .values('name')
+        .annotate(authors_quantity=Count('id'))
+        .order_by('name')
+    )
+    root.entries.extend(
+        [
+            AlphabetTree(name=result['name'], authors_quantity=result['authors_quantity']) for result in results
+        ]
+    )
+
+    populate_branches(tree=root, level=2)
+
+    # digits and other symbols are at the end of the list
+    result = (
+        Author.objects
+        .filter(last_name__regex=r'^[0-9]')
+        .count()
+    )
+
+    if result > 0:
+        root.entries.append(AlphabetTree(
+            name='0-9',
+            regex=r'^[0-9]',
+            authors_quantity=result,
+        ))
+
+    result = (
+        Author.objects
+        .filter(last_name__regex=r'^[^[:alpha:][:digit:]]')
+        .count()
+    )
+
+    if result > 0:
+        root.entries.append(AlphabetTree(
+            name='Other symbols',
+            regex=r'^[^[:alpha:][:digit:]]',
+            authors_quantity=result,
+        ))
+
+    return root
+
+
+def populate_branches(tree: AlphabetTree, level: int, min_quantity: int = 50) -> None:
+    """
+    Populate the branches of the tree with authors grouped by the first letters of their last names.
+    :param tree: the tree to populate
+    :param level: the level of the tree to populate (2 - by the first two letters, etc.)
+    :param min_quantity: the minimum quantity of authors in a branch to populate it with sub-branches.
+            If the quantity of authors in a branch is less than min_quantity,
+            the branch will not be populated with sub-branches.
+            This is done to avoid creating too many branches with a small number of authors in them.
+    """
+    for element in tree.entries:
+        if element.authors_quantity > min_quantity:
+            # only letters after the first letters
+            results = (
+                Author.objects
+                .filter(last_name__iregex=r'^' + element.name + r'[[:alpha:]]')
+                .annotate(name=Left(Lower('last_name'), level))
+                .values('name')
+                .annotate(authors_quantity=Count('id'))
+                .order_by('name')
+            )
+            element.entries.extend(
+                [
+                    AlphabetTree(name=result['name'], authors_quantity=result['authors_quantity']) for result in results
+                ]
+            )
+
+            if level < MAX_TREE_DEPTH:
+                populate_branches(element, level + 1, min_quantity)
+
+            # other symbols or nothing after the first letters
+            # regex = r'^' + element.name + r'[^[:alpha:]]?'
+            regex = r'^' + element.name + r'([^[:alpha:]].*)?$'
+            result = (
+                Author.objects
+                .filter(last_name__iregex=regex)
+                .count()
+            )
+
+            if result > 0:
+                element.entries.append(AlphabetTree(
+                    name=element.name + '*',
+                    regex=regex,
+                    authors_quantity=result,
+                ))
 
 
 def get_or_create_author(flibusta_author: FlibustaAuthor, main_author: Author = None) -> Author:
@@ -90,3 +215,5 @@ def update_genres_from_flibusta():
             )
             if created:
                 logging.info(f'Created new genre {genre}')
+
+
