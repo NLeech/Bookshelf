0243dca11c60d8c896284751d3d1cc4bee503c2b
Olexandr Brailovskyi <alexander.braylovskiy@gmail.com>
2026-03-14 19:39:49 +0200

    feat: enhance admin search functionality by including ID in search fields

--- bookshelf/flibusta/admin.py
+++ bookshelf/flibusta/admin.py
@@ -27,7 +27,7 @@ class FlibustaBookSequenceInline(admin.TabularInline):
 @admin.register(FlibustaAuthor)
 class FlibustaAuthorAdmin(admin.ModelAdmin):
     list_display = ('id', 'last_name', 'first_name', 'middle_name', 'nickname', 'uid')
-    search_fields = ('last_name', 'first_name', 'middle_name', 'nickname', 'email')
+    search_fields = ('id', 'last_name', 'first_name', 'middle_name', 'nickname', 'email')
     list_filter = ('gender',)
     ordering = ('last_name', 'first_name')
 
@@ -35,7 +35,7 @@ class FlibustaAuthorAdmin(admin.ModelAdmin):
 @admin.register(FlibustaGenre)
 class FlibustaGenreAdmin(admin.ModelAdmin):
     list_display = ('id', 'genre_code', 'genre_desc', 'genre_meta')
-    search_fields = ('genre_code', 'genre_desc', 'genre_meta')
+    search_fields = ('id', 'genre_code', 'genre_desc', 'genre_meta')
     list_filter = ('genre_meta',)
     ordering = ('genre_code',)
 
@@ -43,14 +43,14 @@ class FlibustaGenreAdmin(admin.ModelAdmin):
 @admin.register(FlibustaSequence)
 class FlibustaSequenceAdmin(admin.ModelAdmin):
     list_display = ('id', 'name')
-    search_fields = ('name',)
+    search_fields = ('id', 'name',)
     ordering = ('name',)
 
 
 @admin.register(FlibustaBook)
 class FlibustaBookAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'lang', 'file_type', 'year', 'deleted')
-    search_fields = ('title', 'title1', 'md5')
+    search_fields = ('id', 'title', 'title1', 'md5')
     list_filter = ('lang', 'file_type', 'deleted', 'year')
     inlines = [FlibustaBookAuthorInline, FlibustaBookGenreInline, FlibustaBookSequenceInline]
     ordering = ('-id',)
